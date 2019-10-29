7297911a06d001b3afb6a4db426e14ed33f0f903
berlinen <1292393108@qq.com>
2019-10-29 16:40:16 +0800

    test

--- .prettierrc.js
+++ .prettierrc.js
@@ -3,6 +3,10 @@ module.exports = {
     jsxBracketSameLine: true,
     singleQuote: true,
     trailingComma: 'all',
+    tabWidth: 4,
+    printWidth: 120,
+    requirePragma: false, // 是否要注释来决定是否格式化代码
+    arrowParens: 'avoid', // (x) => {} 是否要有小括号
     // parser: '@typescript-eslint/parser',
     // extends: [
     //   'plugin:@typescript-eslint/recommended',
