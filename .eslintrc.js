5a383a29b95575fa6d909156afbe6d24acc3caaa
berlinen <1292393108@qq.com>
2019-10-29 16:50:12 +0800

    test

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,40 +1,39 @@
 module.exports = {
-  root: true,
-  extends: '@react-native-community',
-  parser: "@typescript-eslint/parser",
-  extends: ["plugin:@typescript-eslint/recommended",  'eslint-config-alloy/typescript'],
-  plugins: ["@typescript-eslint", "react"],
-  rules: {
-        'indent': [
-          2,
-          4,
-          {
-              "SwitchCase": 2
-          }
-        ],
-        "semi": [
+    root: true,
+    parser: '@typescript-eslint/parser',
+    extends: ['@react-native-community', 'plugin:@typescript-eslint/recommended', 'eslint-config-alloy/typescript'],
+    plugins: ['@typescript-eslint', 'react'],
+    rules: {
+        indent: [
             2,
-            "always",
+            4,
             {
-                "omitLastInOneLineBlock": true
-            }
+                SwitchCase: 2,
+            },
         ],
-         // @fixable 必须使用 === 或 !==，禁止使用 == 或 !=，与 null 比较时除外
-         'eqeqeq': [
+        semi: [
+            2,
+            'always',
+            {
+                omitLastInOneLineBlock: true,
+            },
+        ],
+        // @fixable 必须使用 === 或 !==，禁止使用 == 或 !=，与 null 比较时除外
+        eqeqeq: [
             'error',
             'always',
             {
-                null: 'ignore'
-            }
+                null: 'ignore',
+            },
         ],
-  },
-  // 跟路径引入
-  settings: {
-    'import/resolver': {
-      'babel-plugin-root-import': {
-        rootPathSuffix: 'src',
-        rootPathPrefix: '~',
-      },
     },
-  },
+    // 跟路径引入
+    settings: {
+        'import/resolver': {
+            'babel-plugin-root-import': {
+                rootPathSuffix: 'src',
+                rootPathPrefix: '~',
+            },
+        },
+    },
 };
