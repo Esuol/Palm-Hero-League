ae82f4fc4769b97d3ef90af8e5ab54c529c6f1b2
berlinen <1292393108@qq.com>
2019-10-29 14:47:40 +0800

    eslint和prettierrc配置

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,10 @@
 module.exports = {
   root: true,
   extends: '@react-native-community',
+  parser: "@typescript-eslint/parser",
+  extends: ["plugin:@typescript-eslint/recommended", "react-app"],
+  plugins: ["@typescript-eslint", "react"],
+  rules: {},
   // 跟路径引入
   settings: {
     'import/resolver': {
--- .prettierrc.js
+++ .prettierrc.js
@@ -3,4 +3,12 @@ module.exports = {
   jsxBracketSameLine: true,
   singleQuote: true,
   trailingComma: 'all',
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'react-app',
+    'plugin:prettier/recommended',
+  ],
+  plugins: ['@typescript-eslint', 'react'],
+  rules: {},
 };
