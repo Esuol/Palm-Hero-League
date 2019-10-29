7762b45ae6e49024f9b7cf426e7c72bfa8912dda
berlinen <1292393108@qq.com>
2019-10-29 17:23:19 +0800

    prettierrc配置

--- .prettierrc.js
+++ .prettierrc.js
@@ -5,9 +5,9 @@ module.exports = {
     trailingComma: 'all',
     tabWidth: 4,
     printWidth: 120,
-    requirePragma: false, // 是否要注释来决定是否格式化代码
+    requirePragma: true, // 是否要注释来决定是否格式化代码
     arrowParens: 'avoid', // (x) => {} 是否要有小括号
-
+    proseWrap: "always",
     // parser: '@typescript-eslint/parser',
     // extends: [
     //   'plugin:@typescript-eslint/recommended',
