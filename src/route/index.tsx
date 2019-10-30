ed5b7ed5864c0eb6606471197b2c727aa3be7b4f
berlinen <1292393108@qq.com>
2019-10-30 18:27:22 +0800

    Component definition is missing display nameeslint

--- src/route/index.tsx
+++ src/route/index.tsx
@@ -16,29 +16,31 @@ interface Props {
     tintColor: string;
 }
 
+const tabbarFunc: (props: Props, name: string) => React.ReactNode  = (props: Props, name: string) => <Icon name={name} color={props.tintColor} />;
+
 const TabNavigator = createBottomTabNavigator({
     HomeScreen: {
         screen: HomeScreen,
         navigationOptions: {
-            tabBarIcon: (props: Props) => <Icon name="home" color={props.tintColor} />
+            tabBarIcon: (props: Props) => tabbarFunc(props, "home")
         }
     },
     SearchScreen: {
         screen: SearchScreen,
         navigationOptions: {
-            tabBarIcon: (props: Props) => <Icon name="search" color={props.tintColor} />
+            tabBarIcon: (props: Props) => tabbarFunc(props, "search")
         }
     },
     FavoritesScreen: {
         screen: FavoritesScreen,
         navigationOptions: {
-            tabBarIcon: (props: Props) => <Icon name="favorites" color={props.tintColor} />
+            tabBarIcon: (props: Props) =>  tabbarFunc(props, "favorites")
         }
     },
     ProfileScreen: {
         screen: ProfileScreen,
         navigationOptions: {
-            tabBarIcon: (props: Props) => <Icon name="profile" color={props.tintColor} />
+            tabBarIcon: (props: Props) => tabbarFunc(props, "profile")
         }
     }
 },
