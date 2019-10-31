cde3f8bab1fb3f8344d28b046f05147c39359621
berlinen <1292393108@qq.com>
2019-10-31 09:42:19 +0800

    完善tabbar

--- src/components/Tabbar.tsx
+++ src/components/Tabbar.tsx
@@ -27,16 +27,32 @@ const SpotLight = posed.View({
     route2: { x: tabWidth * 2 },
     route3: { x: tabWidth * 3 }
 });
+const Scaler = posed.View({
+    active: { scale: 1.25 },
+    inactive: { scale: 1 }
+});
 
 const S = StyleSheet.create({
-    container: { flexDirection: "row", height: 52, elevation: 2 },
-    tabButton: { flex: 1, justifyContent: "center", alignItems: "center" },
+    container: {
+        flexDirection: "row",
+        height: 52,
+        elevation: 2,
+        alignItems: "center"
+    },
+    tabButton: { flex: 1 },
     spotLight: {
         width: tabWidth,
         height: "100%",
-        backgroundColor: "rgba(128,128,255,0.2)",
-        borderRadius: 8
-    }
+        justifyContent: "center",
+        alignItems: "center"
+    },
+    spotLightInner: {
+        width: 48,
+        height: 48,
+        backgroundColor: "#ee0000",
+        borderRadius: 24
+    },
+    scaler: { flex: 1, alignItems: "center", justifyContent: "center" }
 });
 
 const TabBar: React.SFC<Props> = (props: Props) => {
@@ -60,8 +76,11 @@ const TabBar: React.SFC<Props> = (props: Props) => {
                 <View>
                     <View style={S.container}>
                         <View style={StyleSheet.absoluteFillObject}>
-                            <SpotLight style={S.spotLight} pose={`route${activeRouteIndex}`} />
+                            <SpotLight style={S.spotLight} pose={`route${activeRouteIndex}`}>
+                                <View style={S.spotLightInner} />
+                            </SpotLight>
                         </View>
+
                         {routes.map((route: RouteItem, routeIndex: number) => {
                             const isRouteActive = routeIndex === activeRouteIndex;
                             const tintColor = isRouteActive ? activeTintColor : inactiveTintColor;
@@ -78,9 +97,10 @@ const TabBar: React.SFC<Props> = (props: Props) => {
                                     }}
                                     accessibilityLabel={getAccessibilityLabel({ route })}
                                 >
-                                    {renderIcon({ route, focused: isRouteActive, tintColor })}
-
-                                    <Text>{getLabelText({route})}</Text>
+                                    <Scaler style={S.scaler} pose={isRouteActive ? "active" : "inactive"}>
+                                        {renderIcon({ route, focused: isRouteActive, tintColor })}
+                                    </Scaler>
+                                    {/* <Text>{getLabelText({route})}</Text> */}
                                 </TouchableOpacity>
                             );
                         })}
